b4e499948a0997d384d6a9ac2e31ad9b1827ffec
gabrielpaloni <gabrielsbpaloni@gmail.com>
2025-04-24 15:18:54 -0300

    Mudança de diretório apenas.

--- src/Projetos/SistemaBanco/Main.java
+++
@@ -1,9 +0,0 @@
-package Projetos.SistemaBanco;
-
-import Projetos.SistemaBanco.controller.MainController;
-
-public class Main {
-    public static void main(String[] args) {
-        new MainController();
-    }
-}
